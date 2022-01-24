1f88e33fa9475e45db8dff5d1a18d5b6f4536d7f
Szymon Gos <szymonngos@gmail.com>
2022-01-24 21:59:23 +0000

    add diiscipline buttons

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Button({icon, name}) {
+  return <button className="button" role="button">
+  {icon}
+  <span>{name}</span>
+  </button>;
+}
--- src/components/home/HeroBanner.js
+++ src/components/home/HeroBanner.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Button from '../Button';
+import { IoIosFootball, IoMdBasketball, IoMdTennisball } from 'react-icons/io';
+import { FaVolleyballBall } from 'react-icons/fa';
+import heroBanner from '../../assets/images/basketball.jpg'
 
 export default function HeroBanner() {
   return (
@@ -10,6 +14,18 @@ export default function HeroBanner() {
         <p className='hero__intro'>
         Some amazing introduction for our website built in React with server-side from aws.
         </p>
+      </div>
+      <div className='hero__buttons'>
+        <Button icon={<IoIosFootball className='hero__icon' />} name="football"/>
+        <Button icon={<IoMdBasketball className='hero__icon' />} name="basketball"/>
+        <Button icon={<FaVolleyballBall className='hero__icon' />} name="volleyball"/>
+        <Button icon={<IoMdTennisball className='hero__icon' />} name="tennis"/>
+      </div>
+      <div className='banner'>
+        <img 
+        src={heroBanner} 
+        className='banner__img'
+        alt="basketball" />
       </div>
     </section>
   );
