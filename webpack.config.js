5ea7d1396f2b5bc0990f94add98c4d574325980e
Szymon Gos <szymonngos@gmail.com>
2022-01-24 22:00:22 +0000

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,80 @@
 const webpack = require('webpack');
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 const LinkTypePlugin = require('html-webpack-link-type-plugin').HtmlWebpackLinkTypePlugin;
-require('dotenv').config({ path: './.env' }); 
+require('dotenv').config({ path: './.env' });
 
 module.exports = {
-    output: {
-        path: path.join(__dirname, '/build'),
-        filename: 'index.bundle.js',
-    },
-    devServer: {
-        port: 8080,
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
+  output: {
+    filename: 'index.bundle.js',
+    path: path.join(__dirname, '/build'),  
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images'
             },
-            {
-                test: /\.(sass|scss)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpe?g)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'images'
-                        },
-                    }
-                ]
-            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                quality: 70,                
+                progressive: true,
+              },
+            }
+          }
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.css',
-        }),
-        new LinkTypePlugin({
-            '*.css' : 'text/css'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'body',
-        }),
-        // new CopyPlugin({
-        //     patterns: [
-        //         { from: 'src/assets/images', to: 'images' }
-        //     ]
-        // }),
-        new webpack.DefinePlugin({
-            "process.env": JSON.stringify(process.env)
-          }),
-    ],
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new LinkTypePlugin({
+      '*.css': 'text/css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'src/assets/images', to: 'images' }
+    //   ]
+    // }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env)
+    }),
+  ],
 };
