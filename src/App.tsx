9565d268fb97f5439503c78c245a0e6d66965298
Szymon Gos <szymonngos@gmail.com>
2022-02-05 18:19:21 +0000

    add typescript / fix tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <Router>
-      <Navbar />      
+      <Navbar />  
+      <Home />    
     </Router>
   );
 }
