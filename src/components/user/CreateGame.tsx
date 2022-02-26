fe48afcddf2a0c3c014a1967729d3278f1991d36
Szymon Gos <szymonngos@gmail.com>
2022-02-26 16:37:12 +0000

    add user panel

---
+++ src/components/user/CreateGame.tsx
@@ -0,0 +1,12 @@
+import Container from '../Container';
+
+export default function User() {
+  
+  return (
+    <section className='mt-10'>
+      <Container>                
+        Create a Game
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/user/Profile.tsx
@@ -0,0 +1,12 @@
+import Container from '../Container';
+
+export default function User() {
+  
+  return (
+    <section className='mt-10'>
+      <Container>                
+        User Profile
+      </Container>
+    </section>
+  )
+}
