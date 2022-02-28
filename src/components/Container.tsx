2b9e6467128d7eb90be6d7d0de6d01b9ba644210
Szymon Gos <szymonngos@gmail.com>
2022-02-28 09:50:22 +0000

    add form

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,3 @@
-export default function Container({ children }: any) {
-  return <div className='grid grid-cols-4 gap-x-4 px-3 text-base sm:grid-cols-12 sm:gap-x-6 sm:px-10 '>{children}</div>
+export default function Container({ className, children }: { className?: string; children: any}) {
+  return <div className={`grid grid-cols-4 gap-x-4 px-3 text-base sm:grid-cols-12 sm:gap-x-6 sm:px-10 ${className}`}>{children}</div>
 }
--- src/components/user/CreateGame.tsx
+++ src/components/user/CreateGame.tsx
@@ -1,12 +1,33 @@
-import Container from '../Container';
+import Container from '../Container'
 
 export default function User() {
-  
   return (
-    <section className='mt-10'>
-      <Container>                
-        Create a Game
-      </Container>
-    </section>
+    <Container className='px-0 sm:px-0'>
+      <div className='col-span-full'>New Game</div>
+      <form className='col-span-full md:col-span-6 border-2 '>
+        <div className='flex flex-col'>
+          <label>title</label>
+          <input type='text' required placeholder='add title' />
+        </div>
+        <div className='flex flex-col'>
+          <label>Select Discipline</label>
+          <select>
+            <option>football</option>
+            <option>basketball</option>
+            <option>volleyball</option>
+            <option>tennis</option>
+          </select>
+        </div>
+        <div className='flex flex-col'>
+          <label>Select Venue</label>
+          <select>
+            <option>venue option</option>
+            <option>venue option</option>
+            <option>venue option</option>
+            <option>venue option</option>
+          </select>
+        </div>
+      </form>
+    </Container>
   )
 }
